1c2166789bf36f0440ddd1ec2cd4abf9cea4dbf6
j45186891-commits <j45186891@gmail.com>
2025-10-08 18:53:17 +0100

    Add files via upload

---
+++ create-admin.js
@@ -0,0 +1,13 @@
+const bcrypt = require('bcrypt');
+
+// Generate hash for password "admin123"
+const password = 'admin123';
+const saltRounds = 10;
+
+bcrypt.hash(password, saltRounds, (err, hash) => {
+  if (err) {
+    console.error('Error hashing password:', err);
+    return;
+  }
+  console.log('Hashed password:', hash);
+});
